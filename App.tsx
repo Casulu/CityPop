c6de2f659bf0aad916c13f7445d6f47d0f0d3bfc
Rasmus Lyxell <c19rll@cs.umu.se>
2022-04-30 00:54:21 +0200

    Created basic homepage. in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.appTitle}>CityPop</Text>
+      <View style={styles.homeButtonContainer}>
+        <Button 
+          onPress={() => console.log("Hej")} 
+          title="SEARCH BY CITY"/>
+        <Button 
+          onPress={() => console.log("Hej")} 
+          title="SEARCH BY COUNTRY"/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  appTitle: {
+    padding: 100,
+    fontSize: 32
+  },
+  homeButtonContainer: {
+
   },
 });
