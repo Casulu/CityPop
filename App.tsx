1e7e1c6eac0f19fb6f5450d81c03e339c3f28158
Rasmus Lyxell <rasmuslyxell@gmail.com>
2022-05-01 22:04:04 +0200

    Changed buttons to TouchableOpacities with styling

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.appTitle}>CityPop</Text>
       <View style={styles.homeButtonContainer}>
-        <Button 
-          onPress={() => console.log("Hej")} 
-          title="SEARCH BY CITY"/>
-        <Button 
-          onPress={() => console.log("Hej")} 
-          title="SEARCH BY COUNTRY"/>
+        <TouchableOpacity 
+          onPress={() => console.log("Hej")}
+          style={styles.touchables}
+        >
+            <Text>SEARCH BY CITY</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => console.log("Hej")}
+          style={styles.touchables}
+        >
+          <Text>SEARCH BY COUNTRY</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -29,9 +36,18 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 120,
     fontSize: 32
+    
   },
   homeButtonContainer: {
     flex: 2,
     alignSelf: 'stretch',
   },
+  touchables: {
+    backgroundColor: '#fad',
+    borderRadius: 3,
+    padding: 20,
+    margin: 2,
+    marginBottom: 5,
+    marginTop: 5,
+  }
 });
