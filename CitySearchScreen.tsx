a20b312525bb90c07e1568768081ac037b5430dd
Rasmus Lyxell <rasmuslyxell@gmail.com>
2022-05-02 22:50:34 +0200

    Changed to a stylesheet

--- CitySearchScreen.tsx
+++ CitySearchScreen.tsx
@@ -5,54 +5,55 @@ import { StyleSheet, Text,  TextInput,  TouchableOpacity, View, Image } from 're
 
 const  CitySearchScreen = ({ navigation }) => {
   return (
-    <View style={{
-      flex: 1,
-      flexDirection: "column",
-      backgroundColor: '#fff',
-      alignItems: 'center',
-    }}>
-      <Text
-        style={{
-          flex: 1,
-          marginTop: '15%',
-          fontSize: 32,
-          textAlign: 'center'
-        }}
-      >
+    <View style={styles.topView}>
+      <Text style={styles.mainText}>
         SEARCH BY 
         {'\n'}
         CITY
       </Text>
-      <View
-        style={{
-          flex: 2,
-          alignSelf: 'stretch'
-        }} 
-      >
-        <TextInput style={{
-          height: 50,
-          fontSize: 25,
-          margin: 10,
-          borderColor: '#fad',
-          borderWidth: 2,
-          borderRadius: 5,
-        }}/
-        >
+      <View style={{flex: 2, alignSelf: 'stretch'}}>
+      <TextInput style={styles.input}/>
       </View>
-      <View onTouchEnd={()=>{}} 
-          style={{
-            backgroundColor: '#fad',
-            width: 50,
-            height: 50,
-            borderRadius: 25,
-            alignContent: 'center',
-            justifyContent: 'center'
-        }}>
+      <TouchableOpacity style={styles.searchButton}>
           <Image source={require('./assets/magnifying-glass.png')} style={{flex: 1, width: 20, height: 20}}/>
-        </View>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  topView: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  mainText: {
+    flex: 1,
+    marginTop: '15%',
+    fontSize: 32,
+    textAlign: 'center'
+  },
+  inputView: {
+
+  },
+  input: {
+    height: 50,
+    fontSize: 25,
+    margin: 10,
+    borderColor: '#fad',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  searchButton: {
+    height: 50,
+    width: 50,
+    fontSize: 25,
+    margin: 10,
+    backgroundColor: '#fad',
+    borderRadius: 25,
+  }
+})
 
 export default CitySearchScreen;
