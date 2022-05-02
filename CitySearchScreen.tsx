8c4b777a8d3e490648a030e30b15cd87d885c758
Rasmus Lyxell <rasmuslyxell@gmail.com>
2022-05-02 22:40:42 +0200

    Tried to add search button

--- CitySearchScreen.tsx
+++ CitySearchScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text,  TextInput,  TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text,  TextInput,  TouchableOpacity, View, Image } from 'react-native';
 
 const  CitySearchScreen = ({ navigation }) => {
   return (
@@ -15,10 +15,13 @@ const  CitySearchScreen = ({ navigation }) => {
         style={{
           flex: 1,
           marginTop: '15%',
-          fontSize: 32
+          fontSize: 32,
+          textAlign: 'center'
         }}
       >
-        SEARCH BY CITY
+        SEARCH BY 
+        {'\n'}
+        CITY
       </Text>
       <View
         style={{
@@ -33,10 +36,20 @@ const  CitySearchScreen = ({ navigation }) => {
           borderColor: '#fad',
           borderWidth: 2,
           borderRadius: 5,
-          
-        }}/>
+        }}/
+        >
       </View>
-      
+      <View onTouchEnd={()=>{}} 
+          style={{
+            backgroundColor: '#fad',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            alignContent: 'center',
+            justifyContent: 'center'
+        }}>
+          <Image source={require('./assets/magnifying-glass.png')} style={{flex: 1, width: 20, height: 20}}/>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
