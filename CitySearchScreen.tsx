85b2ac25c0b41c620130146e69a0e547fb91a1c0
Rasmus Lyxell <rasmuslyxell@gmail.com>
2022-05-02 20:09:19 +0200

    Added text and input field

--- CitySearchScreen.tsx
+++ CitySearchScreen.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text,  TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text,  TextInput,  TouchableOpacity, View } from 'react-native';
 
 const  CitySearchScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>City</Text>
+    <View style={{
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: '#fff',
+      alignItems: 'center',
+    }}>
+      <Text
+        style={{
+          flex: 1,
+          marginTop: '10%',
+          fontSize: 32
+        }}
+      >
+        SEARCH BY CITY
+      </Text>
+      <View
+        style={{
+          flex: 2,
+          alignSelf: 'stretch'
+        }} 
+      >
+        <TextInput style={{
+          height: 50,
+          fontSize: 25,
+          margin: 10,
+          borderColor: '#fad',
+          borderWidth: 2,
+          borderRadius: 5,
+          
+        }}/>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
