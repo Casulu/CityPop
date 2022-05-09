edab1f01bc550ad9951128ff21b49d80db4f5197
Rasmus Lyxell <c19rll@cs.umu.se>
2022-05-09 23:48:34 +0200

    Started creating a common stylesheet

---
+++ CommonStyles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native"
+
+export const common = StyleSheet.create({
+    titleText: {
+        marginTop: '15%',
+        fontSize: 32,
+        textAlign: 'center'
+    },
+    buttonText: {
+        fontSize: 15,
+        textAlign: 'center'
+    }
+})
